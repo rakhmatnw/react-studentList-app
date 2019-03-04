fccabd988fe5c37807482cd48a95f9a2627fcb75
rakhmatnw <rakhmatnashrul@gmail.com>
2019-03-04 15:09:03 +0700

    [rakhmatnw] - I create studentTable component for showing the student data

--- src/components/studentList/StudentListContainer.js
+++ src/components/studentList/StudentListContainer.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import StudentTable from './tables/StudentTable';
+
 
 class StudentListContainer extends Component {
+  state = {
+    students: [
+      {id:0, name: 'Ade', class:'back-end', contact:'0123'}
+    ]
+  }
 
   render(){
     return(
       <div className="container text-center">
         <h1 className="mb-5">Student List</h1>
+        <StudentTable students={this.state.students}/>
       </div>
     )
   }
